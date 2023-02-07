d37f0bbfe6cbe1d49928e1b80dc17712fe523b1b
Fredrik Brännbacka <fredrik.brannbacka@ilpvfx.com>
2023-02-07 11:12:23 +0100

    Remove locals

--- modules/repositories/main.tf
+++ modules/repositories/main.tf
@@ -11,23 +11,15 @@ variable "repository" {
     ))
 }
 
-
-
-locals {
-  enriched_repositories = {
-        for index, repo in var.repository:
-        repo.name => merge(repo, {
-            topics = distinct(concat(repo.topics, var.default_topics))
-        })
-    }
-}
-
 resource "github_repository" "repository" {
-    for_each = local.enriched_repositories
+    for_each = {
+        for repo in var.repository:
+        repo.name => repo
+    }
     name = each.value.name
-    topics = each.value.topics
+    topics = distinct(concat(each.value.topics, var.default_topics))
 }
 
 output "repositories" {
-  value = local.enriched_repositories
+  value = var.repository
 }
